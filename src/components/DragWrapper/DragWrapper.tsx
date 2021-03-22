d6b4299023518bb8d434d2c30cd70e063418e366
mormotomyia <mormotomyia@mormotomyia.de>
2021-03-22 22:52:16 +0100

    DragWrapper

--- src/components/DragWrapper/DragWrapper.tsx
+++ src/components/DragWrapper/DragWrapper.tsx
@@ -24,33 +24,87 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
   const element:any = useRef()
 
   const onDrag = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    if(element.current !== undefined) {
-      const xPos = Math.trunc((e.clientX) / currentScale - clickDelta.current.x)
-      const yPos = Math.trunc((e.clientY) / currentScale - clickDelta.current.y)
-      // element?.current?.setAttribute('style', `left:${xPos}px; top:${yPos}px`)
-      element?.current?.setAttribute('style', `transform:translate(${xPos}px, ${yPos}px);`)
-      callback({x:xPos, y:yPos})
+    e.preventDefault();
+    e.stopPropagation();
+    if (element.current !== undefined) {
+        const xPos = Math.trunc(
+            e.clientX / currentScale - clickDelta.current.x
+        );
+        const yPos = Math.trunc(
+            e.clientY / currentScale - clickDelta.current.y
+        );
+        // element?.current?.setAttribute('style', `left:${xPos}px; top:${yPos}px`)
+        element?.current?.setAttribute(
+            'style',
+            `transform:translate(${xPos}px, ${yPos}px);`
+        );
+        callback({ x: xPos, y: yPos });
     }
-  }
+};
 
-  const onUp = (e) => {
-    e.preventDefault()
-    document.removeEventListener("mouseup", onUp)
-    document.removeEventListener("mousemove", onDrag)
-  }
+const onDragTouch = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (element.current !== undefined) {
+        const xPos = Math.trunc(
+            e.clientX / currentScale - clickDelta.current.x
+        );
+        const yPos = Math.trunc(
+            e.clientY / currentScale - clickDelta.current.y
+        );
+        // element?.current?.setAttribute('style', `left:${xPos}px; top:${yPos}px`)
+        element?.current?.setAttribute(
+            'style',
+            `transform:translate(${xPos}px, ${yPos}px);`
+        );
+        callback({ x: xPos, y: yPos });
+    }
+};
 
-  const onDown = (e) => {
-    e.preventDefault()
-    const boundingRect = e.currentTarget.getBoundingClientRect()
+const onUp = (e) => {
+    console.log(e);
+    e.preventDefault();
+    document.removeEventListener('mouseup', onUp);
+    document.removeEventListener('mousemove', onDrag);
+};
+
+const onUpTouch = (e) => {
+    console.log(e);
+    e.preventDefault();
+    document.removeEventListener('touchstop', onUpTouch);
+    document.removeEventListener('ontouchmove', onDragTouch);
+};
+
+const onDownTouch = (e) => {
+    e.preventDefault();
+    console.log(e);
+    const boundingRect = e.currentTarget.getBoundingClientRect();
     clickDelta.current = {
-      x: Math.trunc((e.clientX - boundingRect.x + panOffset.x) / currentScale),
-      y: Math.trunc((e.clientY - boundingRect.y + panOffset.y) / currentScale),
-    }
-    document.addEventListener("mouseup", onUp)
-    document.addEventListener("mousemove", onDrag)
-  }
+        x: Math.trunc(
+            (e.clientX - boundingRect.x + panOffset.x) / currentScale
+        ),
+        y: Math.trunc(
+            (e.clientY - boundingRect.y + panOffset.y) / currentScale
+        ),
+    };
+    document.addEventListener('touchstop', onUpTouch);
+    document.addEventListener('ontouchmove', onDragTouch);
+};
+
+const onDown = (e) => {
+    e.preventDefault();
+    const boundingRect = e.currentTarget.getBoundingClientRect();
+    clickDelta.current = {
+        x: Math.trunc(
+            (e.clientX - boundingRect.x + panOffset.x) / currentScale
+        ),
+        y: Math.trunc(
+            (e.clientY - boundingRect.y + panOffset.y) / currentScale
+        ),
+    };
+    document.addEventListener('mouseup', onUp);
+    document.addEventListener('mousemove', onDrag);
+};
 
   useEffect(() => {
     // element?.current?.setAttribute('style', `left:${initPos.x}px; top:${initPos.y}px`)
