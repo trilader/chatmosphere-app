97b0d38f1afe90b3c89c95476216aeacf6a20e21
mormotomyia <mormotomyia@mormotomyia.de>
2021-03-23 18:37:00 +0100

    drag wrapper works for mobile clients

--- src/components/DragWrapper/DragWrapper.tsx
+++ src/components/DragWrapper/DragWrapper.tsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 
@@ -24,7 +25,6 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
   const element:any = useRef()
 
   const onDrag = (e) => {
-    console.log(e)
     e.preventDefault();
     e.stopPropagation();
     if (element.current !== undefined) {
@@ -43,18 +43,28 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
     }
   };
 
+
+
+  const onUp = (e) => {
+    e.preventDefault();
+    document.removeEventListener('mouseup', onUp);
+    document.removeEventListener('mousemove', onDrag);
+  };
+
+  const onUpTouch = (e) => {
+    e.preventDefault();
+    document.removeEventListener('touchstop', onUpTouch);
+    document.removeEventListener('touchmove', onDragTouch);
+    };
+
   const onDragTouch = (e) => {
-    console.log(e)
     e.preventDefault();
     e.stopPropagation();
+    // console.log(`current : ${clickDelta.current.x}, ${clickDelta.current.y}`)
+    // console.log(`screen position : ${e.touches[0].screenX}, ${e.touches[0].screenY}`)
     if (element.current !== undefined) {
-        const xPos = Math.trunc(
-            e.clientX / currentScale - clickDelta.current.x
-        );
-        const yPos = Math.trunc(
-            e.clientY / currentScale - clickDelta.current.y
-        );
-        // element?.current?.setAttribute('style', `left:${xPos}px; top:${yPos}px`)
+        const xPos = Math.trunc(-clickDelta.current.x + e.touches[0].screenX/currentScale)
+        const yPos = Math.trunc(-clickDelta.current.y + (e.touches[0].screenY)/currentScale)
         element?.current?.setAttribute(
             'style',
             `transform:translate(${xPos}px, ${yPos}px);`
@@ -64,31 +74,24 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
     }
   };
 
-  const onUp = (e) => {
-    console.log(e);
-    e.preventDefault();
-    document.removeEventListener('mouseup', onUp);
-    document.removeEventListener('mousemove', onDrag);
-  };
 
-  const onUpTouch = (e) => {
-    console.log(e);
-    e.preventDefault();
-    document.removeEventListener('touchstop', onUpTouch);
-    document.removeEventListener('touchmove', onDragTouch);
-  };
 
   const onDownTouch = (e) => {
-    console.log(e);
     e.preventDefault();
     e.stopPropagation();
     const boundingRect = e.currentTarget.getBoundingClientRect();
+
+    const transform:String = element?.current?.style.transform
+    const xandy = transform.split('(')[1].split(')')[0].split(',').map(value => parseFloat(value.replace('px','')))
+    const x = -xandy[0]
+    const y = -xandy[1]
+    // console.log(x,y)
     clickDelta.current = {
         x: Math.trunc(
-            (e.clientX - boundingRect.x + panOffset.x) / currentScale
+            (e.touches[0].screenX  + x*currentScale) / currentScale
         ),
         y: Math.trunc(
-            (e.clientY - boundingRect.y + panOffset.y) / currentScale
+            (e.touches[0].screenY + y*currentScale) / currentScale
         ),
     };
     document.addEventListener('touchstop', onUpTouch);
@@ -96,7 +99,6 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
   };
 
   const onDown = (e) => {
-    console.log(e)
     e.preventDefault();
     const boundingRect = e.currentTarget.getBoundingClientRect();
     clickDelta.current = {
@@ -111,6 +113,7 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
     document.addEventListener('mousemove', onDrag);
   };
 
+
   useEffect(() => {
     // element?.current?.setAttribute('style', `left:${initPos.x}px; top:${initPos.y}px`)
     element?.current?.setAttribute('style', `transform:translate(${initPos.x}px, ${initPos.y}px);`)
@@ -119,10 +122,12 @@ const DragWrapper = ({initPos={x:0,y:0}, children, callback=(pos)=>null, current
   },[])
 
   return (
-    <DragElement ref={element} onMouseDown={onDown} onTouchStart={onDownTouch} id="DragElement">
+    //   #onMouseDown={onDown}
+    <DragElement ref={element}  onTouchStartCapture={onDownTouch}  id="DragElement">
       {children}
     </DragElement>
   )
 }
 
 export default DragWrapper
+// 2529, y: -2256
