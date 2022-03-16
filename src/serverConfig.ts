52f9bce6fdf99fecce4a7b8ca3f94fc7be75d4ba
Benedikt Ziemons <ben@rs485.network>
2022-03-16 22:35:35 +0100

    Schroedingers-bit Test Jitsi

--- src/serverConfig.ts
+++ src/serverConfig.ts
@@ -3,12 +3,13 @@ import {connectionOptions as defaultOptions} from './serverConfig-example';
 const customConnectionOptions = {
 
   //# if you configured your jitsi installation to use websockets, else using bosh for connection 
-  serviceUrl: process.env.REACT_APP_USE_WEBSOCKET==="true" ? `wss://${process.env.REACT_APP_SERVICE_URL}/xmpp-websocket` : `//${process.env.REACT_APP_SERVICE_URL}/http-bind`,
+  serviceUrl: `https://${process.env.REACT_APP_SERVICE_URL}/http-bind`,
   hosts: {
     domain: `${process.env.REACT_APP_SERVICE_URL}`,
     muc: `conference.${process.env.REACT_APP_SERVICE_URL}`,
     //# (optional) specify the focus component to use (part of the XMPP stuff). defaults to the value of hosts.domain if not set.
     focus: `focus.${process.env.REACT_APP_SERVICE_URL}`,
+    anonymousdomain: `guest.${process.env.REACT_APP_SERVICE_URL}`,
   },
 
   //# External Connect Urlcan speed up connection if you're using bosh; probably https://$JITSI_MEET_WEB_DOMAIN/http-pre-bind
@@ -20,8 +21,8 @@ const customConnectionOptions = {
   //# bosh can be set to the url of the Prosody XMPP server to be contacted via BOSH.
   //# if you don't set this, it will default to /http-bind
   // bosh: `https://${process.env.REACT_APP_SERVICE_URL}/http-bind?room=chatmosphere1234`, // ! if you make your own please omit the "?room=chatmosphere1234" part
-  bosh: `//${process.env.REACT_APP_SERVICE_URL}/http-bind`, // ! if you make your own please omit the "?room=chatmosphere1234" part
-  websocket: `wss://${process.env.REACT_APP_SERVICE_URL}/xmpp-websocket`,
+  bosh: `https://${process.env.REACT_APP_SERVICE_URL}/http-bind`, // ! if you make your own please omit the "?room=chatmosphere1234" part
+  //websocket: `wss://${process.env.REACT_APP_SERVICE_URL}/xmpp-websocket`,
 
   // !! leave this exactly as is !!
   clientNode: 'http://jitsi.org/jitsimeet'
