e0c0d35c713318e7980c29c3ba6e379d2e820bb0
Benedikt Ziemons <ben@rs485.network>
2022-11-02 21:17:06 +0100

    Revert some configuration changes

--- src/utils/secureConferenceName.ts
+++ src/utils/secureConferenceName.ts
@@ -3,5 +3,5 @@ export const secureConferenceName = (name:string|undefined, prefix:string|undefi
   if(!name) throw Error("no Conference Name was provided")
   name = name.toLowerCase()
   name = name.replace(/[~`!@#$%^&*()+={}[\];:'"<>.,/\\?-_]/g, '');
-  return prefix ? prefix + name : _defaultPrefix + name
+  return prefix ? prefix + name : name
 }
